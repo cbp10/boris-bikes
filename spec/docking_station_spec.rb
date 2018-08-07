e5afecadcf37887b6b192ee9130f84ad645c6df8
Lucien Najev <luciennajev@hotmail.com>
2018-08-07 20:30:10 +0100

    unit test added to check class will respond to release_bike method, release_bike method added to dockingstation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 RSpec.describe DockingStation do
 
+  #it { expect(DockingStation.new).to respond_to(:release_bike) } this was my attempt
+  it { is_expected.to respond_to :release_bike }
+
+
 end
