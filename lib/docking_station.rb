b9a8e12d9ae51fe50f1c6005e9a0921cf708ffc9
Lucien Najev <luciennajev@hotmail.com>
2018-08-08 16:15:31 +0100

    updated dock(bike) method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,49 @@ require_relative 'bike'
 
 class DockingStation
 
-
   def release_bike
    Bike.new
+
+  end
+
+  def dock(bike)
+    bike
+  end
+
+  def bike
+
   end
 
 
 
+
+
+
 end
+
+
+
+
+=begin
+
+   attr_reader :count
+
+   @count = number of bikes
+
+   def release_bike
+     @bikes -= 1
+   end
+
+   def dock_bike
+    @bikes += 1
+   end
+
+
+
+
+
+
+
+
+
+=end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,28 @@ RSpec.describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+#  it "releases working bikes" do
+#    bike = subject.release_bike
+#    expect(bike).to be_working
+#  end
+
   it "releases working bikes" do
     bike = subject.release_bike
-    expect(bike).to be_working
+    expect(bike.working?).to eq(true)
   end
+
+
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it {is_expected.to respond_to :bike}
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+
 
 
 end
